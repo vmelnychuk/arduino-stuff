d2eed43afe8006453fa1b5d15189443acda9d88b
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2017-02-04 11:41:34 +0100

    add header file for Telegraph

---
+++ Telegraph/Telegraph.h
@@ -0,0 +1,20 @@
+#ifndef __TELEGRAPH_H__
+#define __TELEGRAPH_H__
+
+class Telegraph {
+    public:
+        Telegraph(const int dit_length);
+        void send_message(const char* message);
+
+    private:
+        void dit();
+        void dah();
+        void output_code(const char* code);
+        void output_symbol(const int length);
+
+        int dit_length;
+        int dah_length;
+};
+
+#endif // __TELEGRAPH_H__
+
